637dbf6345a020f6ff4cc5a6ffb164ab4e119086
ronaldhuynen <ronald.huynen@timebanmk.cc>
2024-02-12 23:13:06 +0100

    npm install

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -5,9 +5,9 @@
 #=================================================
 
 YNH_PHP_VERSION="8.1"
-
-# Composer version
 YNH_COMPOSER_VERSION="2.5.4"
+YNH_NODEJS_VERSION="12.16"
+YNH_NPM_VERSION="8.12"
 
 pkg_dependencies="memcached libmemcached-tools php${YNH_PHP_VERSION}-memcache php${YNH_PHP_VERSION}-memcached php${YNH_PHP_VERSION}-sqlite3 php${YNH_PHP_VERSION}-curl php${YNH_PHP_VERSION}-mbstring php${YNH_PHP_VERSION}-json php${YNH_PHP_VERSION}-gd php${YNH_PHP_VERSION}-mysql"
 
